8dcb392d3d4da44a4ad067023f82ac083fefc53e
Yersultann <yersultan.u@gmail.com>
2018-12-23 13:09:07 +0600

    add data component

--- client/App.vue
+++ client/App.vue
@@ -16,16 +16,24 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+
+
+    <date>20 march</date>
   </div>
 </template>
 
 <script>
+import Data from './Data.vue';
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    date: Data
   }
 }
 </script>
---
+++ client/Data.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="datas">
+ 		<h3> {{ data }} </h3>
+ 	</div>
+</template>
+
+<script>
+export default {
+  name: 'datas',
+  data () {
+    return {
+      data: 'March 2016'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.datas {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #979797;
+  margin-top: 60px;
+  background-image: url('/assests/Oval.png');
+}
+
+</style>
